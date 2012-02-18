9b9cce393daa09c73f09110663d2a4af0ddf65fd
Michal Pokorný <pok@rny.cz>
2012-02-18 13:23:12 +0100

    example na pouziti listu v aktivite

--- src/org/SdkYoungHeads/DoIKnowYou/MainActivity.java
+++ src/org/SdkYoungHeads/DoIKnowYou/MainActivity.java
@@ -1,13 +1,31 @@
 package org.SdkYoungHeads.DoIKnowYou;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends ListActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO: Tady vypsat seznam skupin...
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+				"Linux", "OS/2" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		//String item = (String) getListAdapter().getItem(position);
+		//Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+		
+		
+		// TODO: prejit na detail aktivitu
+	}
 }
