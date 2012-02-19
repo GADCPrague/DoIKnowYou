351d9b4ffaa7e94f15e1b203310f969476878b7f
Barvoj <barvoj@seznam.cz>
2012-02-19 15:22:40 +0100

    Add
    Signed-off-by: Barvoj <barvoj@seznam.cz>

--- src/org/SdkYoungHeads/DoIKnowYou/AddNewGroupActivity.java
+++ src/org/SdkYoungHeads/DoIKnowYou/AddNewGroupActivity.java
@@ -9,6 +9,7 @@ import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
